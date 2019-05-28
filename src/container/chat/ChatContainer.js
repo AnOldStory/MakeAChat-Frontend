1b3422f0eb3845dfedbe9f4e2e9e9ea44aaf1cb5
HyeongChang Lee <hc9904@naver.com>
2019-05-28 15:59:04 +0900

    Update public chat css

--- src/container/chat/ChatContainer.js
+++ src/container/chat/ChatContainer.js
@@ -5,6 +5,8 @@ import LoginLink from "component/LoginLink";
 
 import config from "_variable";
 
+import "./ChatContainer.css";
+
 class ChatContainer extends Component {
   constructor(props) {
     super(props);
@@ -21,19 +23,22 @@ class ChatContainer extends Component {
     this.handleResponseErr = this.handleResponseErr.bind(this);
     this.handleTyping = this.handleTyping.bind(this);
     this.handleMove = this.handleMove.bind(this);
+    this.handleEnter = this.handleEnter.bind(this);
   }
 
   componentDidMount() {
-    console.log("Global Login");
-    /* check global connection */
-    this.socket = io.connect(this.state.serverUrl, {
-      transports: ["websocket"],
-      forceNew: true
-    });
-    /* subscribe chat-pull */
-    this.socket.on("chat-pull", this.handleResponse);
-    this.socket.on("err-pull", this.handleResponseErr);
-    this.socket.emit("get-chat-list");
+    if (this.state.token) {
+      console.log("Global Login");
+      /* check global connection */
+      this.socket = io.connect(this.state.serverUrl, {
+        transports: ["websocket"],
+        forceNew: true
+      });
+      /* subscribe chat-pull */
+      this.socket.on("chat-pull", this.handleResponse);
+      this.socket.on("err-pull", this.handleResponseErr);
+      this.socket.emit("get-chat-list");
+    }
   }
 
   handlePush() {
@@ -59,6 +64,9 @@ class ChatContainer extends Component {
         chats: result,
         err: ""
       });
+      if (this.messagesEnd) {
+        this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+      }
     } else {
       this.setState({
         err: "예상치 못한 오류가 발생하였습니다. 서버 연결을 확인해주세요."
@@ -91,32 +99,63 @@ class ChatContainer extends Component {
     this.props.routeMethod.history.push("/privatechat");
   }
 
+  handleEnter(e) {
+    if (e.key === "Enter" && this.state.token) {
+      this.handlePush();
+    }
+  }
+
   render() {
     return (
-      <div>
-        {this.state.token ? (
-          <>
-            <div onClick={this.handleMove}> 개인공간으로 가기 </div>
-            안녕하세요 {this.state.nickname}님 공용 채팅 공간
-            <div className="chat-send">
-              {this.state.chats.map((msg, i) => {
-                return (
-                  <div key={i}>
-                    {msg.nickname} : {msg.text}
-                  </div>
-                );
-              })}
-              <input onChange={this.handleTyping} value={this.state.text} />
-              <button onClick={this.handlePush}>채팅보내기</button>
-              <div className="err">{this.state.err}</div>
-            </div>
-          </>
-        ) : (
-          <>
-            로그인이 필요합니다.
-            <LoginLink />
-          </>
-        )}
+      <div className="chat_all">
+        <div className="header">Make A Chat</div>
+        <div className="list_area" />
+        <div className="chat_main">
+          <div className="message_area">
+            <span className="leave">
+              <button type="button" name="button1" onClick={this.handleMove}>
+                Go to Personal Room
+              </button>
+            </span>
+            {this.state.token ? (
+              <>
+                안녕하세요 {this.state.nickname}님 여기는 공용 채팅 공간입니다.
+                {this.state.chats.map((msg, i) => {
+                  return (
+                    <div key={i}>
+                      {msg.nickname} : {msg.text}
+                    </div>
+                  );
+                })}
+              </>
+            ) : (
+              <>
+                로그인이 필요합니다.
+                <LoginLink />
+              </>
+            )}
+
+            <div
+              ref={el => {
+                this.messagesEnd = el;
+              }}
+            />
+          </div>
+
+          <div className="input_box">
+            <input
+              type="text"
+              id="text_input"
+              onChange={this.handleTyping}
+              value={this.state.text}
+              onKeyDown={this.handleEnter}
+            />
+          </div>
+          <button type="button" name="button2" onClick={this.handlePush}>
+            SEND
+          </button>
+          <div className="err">{this.state.err}</div>
+        </div>
       </div>
     );
   }
